f3b850df00c8f752feb3b6c4f0627b64ef4c68f1
jaskrrish <jashehz@gmail.com>
2023-10-12 21:40:57 +0530

    PopUp Effect

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 function updateMap() {
-    console.log("Updating Map with RealTime")
+  console.log("Updating Map with RealTime");
   fetch("/data.json")
     .then((response) => response.json())
-    .then(rsp => {
-        // console.log(rsp.data)
-        rsp.data.forEach(element => {
-            latitude=element.latitude
-            longitude=element.longitude
-            //
-            cases =element.infected
-            //
-            if(cases>255){
-                color= "rgb(255, 0, 0)"
-            }
-            else{
-                color = `rgb(${cases},0,0)`
-            }
-            //Map Marker
-            new mapboxgl.Marker({
-                draggable: false,
-                color: color
-                })
-                .setLngLat([longitude, latitude])
-                .addTo(map);
-            //
-            //
-            // Hover Effect
-        });
+    .then((rsp) => {
+      // console.log(rsp.data)
+      rsp.data.forEach((element) => {
+        latitude = element.latitude;
+        longitude = element.longitude;
+        //
+        cases = element.infected;
+        //
+        if (cases > 255) {
+          color = "rgb(255, 0, 0)";
+        } else {
+          color = `rgb(${cases},0,0)`;
+        }
+        //
+        //Pop-up Marker
+        const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<p> PLACE: ${element.name}</p><p> CASES: ${cases}</p>`
+            );
+        //Map Marker
+        new mapboxgl.Marker({
+          draggable: false,
+          color: color,
+        })
+          .setLngLat([longitude, latitude])
+          .addTo(map)
+          .setPopup(popup)
+        //
+        //Hover Effect
+      });
     });
 }
 let interval = 20000;
-setInterval( updateMap, interval)
+setInterval(updateMap, interval);
 // updateMap();
